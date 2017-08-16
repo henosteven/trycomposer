9e87d831b01c72277b04c467088cfd67a9baa2dc
jinjing <liujinjing@didichuxing.com>
2017-08-16 13:03:43 +0800

    add monolog

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,22 @@ class ComposerStaticInit8e33cf69fb2c9f67f12c8b6dfbee39dc
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'M' => 
+        array (
+            'Monolog' => 
+            array (
+                0 => __DIR__ . '/..' . '/monolog/monolog/src',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit8e33cf69fb2c9f67f12c8b6dfbee39dc::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit8e33cf69fb2c9f67f12c8b6dfbee39dc::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit8e33cf69fb2c9f67f12c8b6dfbee39dc::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
